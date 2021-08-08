845118b1ff5cac223adf24b5fd3aa69560eded7c
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-08-08 16:47:10 +0530

    part 2 ex - cool router added

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool',function(req,res,next) {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(`<h2>You're so cool</h2>`)
+  res.end()
+})
+
 module.exports = router;
