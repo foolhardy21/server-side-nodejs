1fc704d9216c169fbe3b5e83356f8b57b2241517
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-08-10 12:40:30 +0530

    mongodb connection setup

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const username = process.argv[2]
+const password = process.argv[3]
+const dbname = process.argv[4]
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.fafup.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
