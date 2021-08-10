487b5877c4a9e9dbfcb412549c1490e98b3da658
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-08-10 16:06:43 +0530

    part 3 - library db reated and documents added

---
+++ models/author.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const AuthorSchema = new Schema({
+    first_name: {type: String, required: true, maxLength: 100},
+    family_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+})
+
+AuthorSchema.virtual('name').get(function() {
+    return this.first_name + ',' + this.family_name
+})
+
+AuthorSchema.virtual('lifespan').get(function() {
+    let lifetime_string = ''
+    if(this.date_of_birth) {
+        lifetime_string = DateTime.fromJSDate(this.date_of_birth)
+                .toLocaleString(DateTime.DATE_MED)
+    }
+    lifetime_string += ' - '
+    if(this.date_of_death) {
+        lifetime_string += DateTime.fromJSDate(this.date_of_death)
+                .toLocaleString(DateTime.DATE_MED)
+    }
+
+    return lifetime_string
+})
+
+AuthorSchema.virtual('url').get(function() {
+    return '/catalog/author/' + this._id
+})
+
+module.exports = mongoose.model('Author', AuthorSchema)
